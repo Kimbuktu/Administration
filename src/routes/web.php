755937c81ea3a2ef9e2c3e21bfd346783c6a80bf
Kimbuktu <kornwinkel.kim@outlook.com>
2021-08-26 07:22:22 +0000

    Fixed: Usage Problem of AdministrationServiceProvider in web.php for ASP Key

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Providers\AdministrationServiceProvider as ASP;
+use Kimbuktu\Administration\App\Providers\AdministrationServiceProvider as ASP;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/' . strtolower(config(ASP::$key . 'route')) , ['\Kimbuktu\Administration\App\Http\Controllers\AdministrationController', 'index']);
